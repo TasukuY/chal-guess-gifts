ca2d6594a0df0d53d782fcd7e6b41b6962524f68
TasukuY <alohaty81@gmail.com>
2022-03-08 16:13:15 -0700

    coding challenge done

---
+++ guessGifts.js
@@ -0,0 +1,34 @@
+
+function guessGifts(wishlist, presents){
+    let gifts = [];
+
+    for(let i = 0; i < presents.length; i++){
+        let size = presents[i].size;
+        let clatters = presents[i].clatters;
+        let weight = presents[i].weight;
+        for(let j = 0; j < wishlist.length; j++){
+            if(wishlist[j].size === size){
+                if(wishlist[j].clatters === clatters){
+                    if(wishlist[j].weight === weight){
+                        gifts.push(wishlist[j].name);
+                    }
+                }
+            }
+        }
+    }
+
+    return gifts;
+}
+
+var wishlist = [
+    {name: "Mini Puzzle", size: "small", clatters: "yes", weight: "light"},
+    {name: "Toy Car", size: "medium", clatters: "a bit", weight: "medium"},
+    {name: "Card Game", size: "small", clatters: "no", weight: "light"}
+];
+
+var presents = [
+    {size: "medium", clatters: "a bit", weight: "medium"},
+    {size: "small", clatters: "yes", weight: "light"}
+];
+
+console.log(guessGifts(wishlist, presents));
